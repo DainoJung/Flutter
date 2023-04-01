2de86eb673b7cc5266cddea9aa3dc03434964753
DainJung <wjdekdls3693@gmail.com>
2023-04-02 02:52:01 +0900

    webtoon app

---
+++ webtoonapp/lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webtoonapp/models/webtoon_model.dart';
+import 'package:webtoonapp/sevice/api_service.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 26,
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text('There is data!');
+          }
+          return const Text('Loading...');
+        },
+      ),
+    );
+  }
+}
---
+++ webtoonapp/lib/sevice/api_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:webtoonapp/models/webtoon_model.dart';
+
+class ApiService {
+  static const String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
+
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
